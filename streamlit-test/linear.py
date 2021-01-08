a30d6e5349a66b57f39a677e6a9b48846a7b4ddc
MURAKI Taichi <muraki.taichi@gmail.com>
2021-01-08 15:31:10 +0900

    Add seaborn.

---
+++ streamlit-test/linear.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sklearn.linear_model import HuberRegressor
+from sklearn.datasets import load_boston #ボストン市の住宅価格データ
+import seaborn as sns
+
+"""
+# ボストン市の住宅価格データ
+"""
+dataset = load_boston()
+df = pd.DataFrame(dataset.data, columns=dataset.feature_names)#説明変数
+df['MEDV_PRICE'] = dataset.target #目的変数を追加
+df
