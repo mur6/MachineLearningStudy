1915efa758239b95fe14badf2acb2c892e295e9e
MURAKI Taichi <muraki.taichi@gmail.com>
2021-01-08 15:47:30 +0900

    analyze boston

--- streamlit-test/linear.py
+++ streamlit-test/linear.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import pandas as pd
 from sklearn.linear_model import HuberRegressor
 from sklearn.datasets import load_boston #ボストン市の住宅価格データ
@@ -10,3 +11,6 @@ dataset = load_boston()
 df = pd.DataFrame(dataset.data, columns=dataset.feature_names)#説明変数
 df['MEDV_PRICE'] = dataset.target #目的変数を追加
 df
+
+pg = sns.pairplot(df)
+st.pyplot(pg)
